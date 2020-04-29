aed8b0a4da803de4878806c5fc69d7a8f6b7b24d
ShamsherAlam <same.ddps@gmail.com>
2020-04-29 11:12:05 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe';
+  name: string;
+  payDate:string;
+  amount:number;
+  height:number;
+  miles:number;
+
+  // data = {
+
+  //   name: "Shamsher Alam",
+  //   college :"TCSC"
+  // };
+
+  onNameChange(value:string){
+    this.name = value;
+  }
+
+  onDateChange(value:string){
+    this.payDate = value;
+  }
+
+  onAmountChange(value:string){
+
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value:string){
+    this.height = parseInt(value);
+  }
+
+  onMilesChange(value:string){
+    if(!value){
+      return this.miles=0;
+    }
+    this.miles = parseFloat(value);
+  }
 }
